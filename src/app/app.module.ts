254b7f8aa1e065a57f2dbcba106530131f9e5122
LanaAlrayes <lanaalrayss@gmail.com>
2025-02-14 14:16:42 +0300

    import the necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppComponent } from './app.component';
 import { SectionComponent } from './section/section.component';
 import { TemplateBuilderComponent } from './template-builder/template-builder.component';
 import { SafeUrlPipe } from './safe-url.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import {MatBadgeModule} from '@angular/material/badge'
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +27,18 @@ import { SafeUrlPipe } from './safe-url.pipe';
     SafeUrlPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatListModule,
+    MatBadgeModule,
+    MatTooltipModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
