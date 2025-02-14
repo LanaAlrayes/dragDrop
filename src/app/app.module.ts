e41af4aad94cd66618a362aca54513cb0f76b282
LanaAlrayes <lanaalrayss@gmail.com>
2025-02-14 12:47:04 +0300

    Create  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SectionComponent } from './section/section.component';
+import { TemplateBuilderComponent } from './template-builder/template-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SectionComponent,
+    TemplateBuilderComponent
   ],
   imports: [
     BrowserModule
