cd19b2a6a1a6d16c3f4c27947e4aa40beeef1b36
LanaAlrayes <lanaalrayss@gmail.com>
2025-02-14 14:36:58 +0300

    add SectionComponent

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
-  styleUrls: ['./section.component.scss']
+  styleUrls: ['./section.component.scss'],
 })
 export class SectionComponent {
+  @Input() section!: { type: string; content: string };
+  @Output() delete = new EventEmitter<void>();
+  @Output() update = new EventEmitter<string>();  // Changed to emit content
+  @Output() updateVideo = new EventEmitter<string>();  // Changed to emit content
 
+  deleteSection(): void {
+    this.delete.emit();
+  }
+// Emit the actual content
+  updateContent(): void {
+    this.update.emit(this.section.content);
+  }
+// Emit the actual content
+  updateContents(): void {
+    this.updateVideo.emit(this.section.content);
+  }
 }
